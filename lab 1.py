48c44126b92772116f0abfa422f0e5b4681e687c
asrin <wic190024@siswa.um.edu.my>
2021-03-25 23:38:02 +0800

    settled

--- lab 1.py
+++ lab 1.py
@@ -99,5 +99,74 @@ def q9():
         print("you guessed too high")
 
 def q10():
-    x = int(input("Enter a number"))
+    num = int(input("Enter a number : "))
+    check = 0
+    for x in range(2,num):
+        if num%x==0:
+            check=1
+            break
+    if check == 0:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
+
+def q11():
+    a=[5,10,15,20,25]
+    print (pop(a))
+
+def pop(x):
+    c=[]
+    c.append(x.pop(0))
+    c.append(x.pop(len(x)-1))
+    return c
+
+def q12():
+    x= int(input("Enter the number to generate fibonacci sequence "))
+    a=[1,1]
+    print (fibonacci(a,x))
+
+def fibonacci(a,x):
+    for y in range (1,x-1):
+        a.append(a[len(a)-2]+a[len(a)-1])
+    return a
+
+def q13():
+    a = [1,4,9,16,25,36,49,4,81,100,9,16,25,36,49]
+    print (unduplicate (a))
+
+def unduplicate(a):
+    b=[]
+    for x in a:
+        if x not in b:
+            b.append(x)
+    return b
+
+def q14():
+    sentence = input("insert a sentence :")
+    x = sentence.split()
+    y = ""
+    for i in x :
+        y=i+" "+y
+    print(y)
+
+def q15():
+    startTime =time.time()
+    question = eval(input("Please enter the password length: "))
+    answer = password(question)
+    print(answer)
+    endTime=time.time()
+    print("Time-Complexity : ",endTime-startTime)  
+def password(userInput):
+    specialCharacter = [random.choice(string.punctuation) for character in range(userInput)]
+    wordLower = [random.choice(string.ascii_lowercase) for lower in range(userInput)]
+    wordUpper = [random.choice(string.ascii_uppercase) for upper in range(userInput)]
+    numbers = [random.choice(string.digits) for number in range(userInput)]
+    generatedPassword = ''.join(specialCharacter + wordLower + wordUpper + numbers)
+    generatedPassword = ''.join(random.choice(generatedPassword) for value in range(userInput))
+    print (str(specialCharacter))
+    return generatedPassword
+
+  
+
+q15()
 
