b55baa8a19139f4d78188c1f4cd9962fd4e5da6c
asrin <wic190024@siswa.um.edu.my>
2021-04-30 23:36:16 +0800

    commit

---
+++ Assignment/Problem3.py
@@ -0,0 +1,96 @@
+import prob1
+import Problem2
+import numpy as np
+from matplotlib import pyplot as plt
+import plotly.graph_objects as go
+
+sent = Problem2.sentiment
+dist = [prob1.packagedTravelled1,prob1.packagedTravelled2,prob1.packagedTravelled3]
+distscore = [[0 for i in range (5)]for i in range(3)]
+
+#std mean dist
+meandist=[]
+stddist=[]
+lowerbound=[]
+upperbound=[]
+
+maxs = max(sent)
+mins = min(sent)
+
+
+for i in range (3):
+    meandist.append(np.mean(dist[i]))
+    stddist.append(np.std(dist[i]))
+    lowerbound.append(meandist[i]-stddist[i])
+    upperbound.append(meandist[i]+stddist[i])
+
+
+print(dist)
+print(meandist)
+print(stddist)
+print(lowerbound)
+print(upperbound)
+
+for i in range (3):
+    for j in range (5):
+        if dist[i][j] > upperbound[i] :
+            distscore[i][j]= 0
+        elif dist[i][j] < lowerbound[i] :
+            distscore[i][j]= maxs-mins
+        else :
+            distscore[i][j]= (maxs-mins)/2
+
+print(distscore)
+
+#std mean sent
+# lowerbound = []
+# upperbound = []
+# meansent = []
+# stdsent = []
+# for i in range (5):
+#     meansent.append(np.mean(sent))
+#     stdsent.append(np.std(sent))
+#     lowerbound.append(meansent[i]-stdsent[i])
+#     upperbound.append(meansent[i]+stdsent[i])
+# print(sent)
+# print(lowerbound,upperbound)
+
+# sentscore = []
+
+# for i in range (5):
+#     if sent[i] > upperbound[i] :
+#         sentscore.append(3)
+#     elif sent[i] < lowerbound[i] :
+#         sentscore.append(1)
+#     else :
+#         sentscore.append(2)
+
+# print (sentscore)
+
+#total score (distance score + sent score)
+totalscore = [[0 for i in range (5)] for i in range (3)]
+for j in range (3):
+    for i in range (5):
+        totalscore[j][i] = sent[i] + distscore[j][i]
+print(totalscore)
+
+total = []
+for j in range (3):
+    n = 0
+    for i in range (5):
+        n += totalscore[j][i]
+    total.append(n)
+print(total)
+
+probability = [[0 for i in range (5)] for i in range (3)]
+for j in range (3):
+    for i in range (5):
+        probability[j][i] = round(((totalscore[j][i]/total[j])),2)
+print(probability)
+
+n =0
+for i in range (3):
+
+    fig = go.Figure([go.Bar(x=["jnt","citylink","dhl","poslaju","gdex"], y=probability[i])])
+    fig.write_html(f'{n}probability.html', auto_open=True)
+    n+=1
--- Assignment/prob1.py
+++ Assignment/prob1.py
@@ -1,30 +1,42 @@
 import gmplot #for google map gui
-from geopy import distance # for shortest distance on earth
-# import googlemaps #to calculate distance on route
 import requests
 import pprint
 
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
+        while low <= high and array[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+      
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    p = partition(array, start, end)
+    quick_sort(array, start, p-1)
+    quick_sort(array, p+1, end)
+
 ############## to create map markers for hubs ##############################
 # Create the map plotter:
-apikey = 'AIzaSyDpGnTkENjkuksN0QzkOG7LzgowI0kavHI' # (your API key here)
-gmap = gmplot.GoogleMapPlotter(3.112585695236, 101.6397000538541, 10,apikey=apikey)#untuk spawn kita masuk awal awal kat mana
+apikey = "AIzaSyDpGnTkENjkuksN0QzkOG7LzgowI0kavHI" # (your API key here)
 
-# hub_lat, hub_long = zip(#zip is to convert touples to list and create a list of touples
-#     *[
-#         (3.0319924887507144,101.37344116244806), (3.112924170027219,101.63982650389863), 
-#         (3.265154613796736,101.68024844550233), (2.9441205329488325,101.7901521759029),
-#         (3.2127230893650065,101.57467295692778)
-#     ]
-# )
+gmap = gmplot.GoogleMapPlotter(3.112585695236, 101.6397000538541, 10,apikey=apikey)#untuk spawn kita masuk awal awal kat mana
 gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
-# gmap.scatter(hub_lat, hub_long,color='cornflowerblue', title="Hubs")
 gmap.marker(3.0319924887507144,101.37344116244806, color='cornflowerblue', title="Port Klang")
 gmap.marker(3.112924170027219,101.63982650389863, color='red', title="Petaling Jaya")
 gmap.marker(3.265154613796736,101.68024844550233, color='orange', title="Batu Caves")
 gmap.marker(2.9441205329488325,101.7901521759029, color='green', title="Kajang")
 gmap.marker(3.2127230893650065,101.57467295692778, color='yellow', title="Sungai Buloh")
-
-gmap.draw("C:\\Users\\USER\\OneDrive - Universiti Malaya\\Desktop\\Sem 4\\ALGO\\Algo assignment\\hubs.html")
+gmap.draw("C:\\Users\\asrin\\Documents\\Lab-Algo\\Assignment\\hubs.html")
 
 ############# To calculate distance customer origin and destination ##############################
 customer_ori_lat,customer_ori_long= zip(#zip is to convert touples to list and create a list of touples
@@ -46,7 +58,9 @@ hub_lat, hub_long = zip(#zip is to convert touples to list and create a list of 
         (3.2127230893650065,101.57467295692778)
     ]
 )
+
 customerDistances=[0]*len(list(customer_ori_lat))
+
 
 for i in range(len(list(customer_ori_lat))):
     customer_ori = str(customer_ori_lat[i])+","+str(customer_ori_long[i])
@@ -55,15 +69,17 @@ for i in range(len(list(customer_ori_lat))):
     output = requests.get(url).json()
     for a in output["rows"]:
         for b in a["elements"]:
-            print("customer "+str(i)+": "+b["distance"]["text"])
+            print("customer "+str(i)+" distance from origin straight to destination: "+b["distance"]["text"])
             customerDistances[i]=b["distance"]["value"]
 
-print(customerDistances)
-
 ############ To calculate distance from origin to hub and hub to destination #########################
-packagedTravelled1=[0]*len(hub_lat)
+packagedTravelled1=[0]*len(hub_lat)#this array is used to be sorted
 packagedTravelled2=[0]*len(hub_lat)
 packagedTravelled3=[0]*len(hub_lat)
+hubIndex1=[0]*len(hub_lat)#use to find the index of the long and lat of hub shortest based on 0th index of the above array
+hubIndex2=[0]*len(hub_lat)
+hubIndex3=[0]*len(hub_lat)
+hubs=["Port Klang","Petaling Jaya","Batu Caves","Kajang","Sungai Buloh"]
 
 for i in range(len(customer_ori_lat)):
     originToHub=0
@@ -86,11 +102,51 @@ for i in range(len(customer_ori_lat)):
                 hubToDestination=b["distance"]["value"]
 
         if i==0:
-            packagedTravelled1[i]=float(originToHub)+float(hubToDestination)
-            print("total distance 1: ",packagedTravelled1[i]," meters")
+            packagedTravelled1[j]=float(originToHub)+float(hubToDestination)
+            hubIndex1[j]=packagedTravelled1[j]
+            print("total distance 1: ",packagedTravelled1[j]," meters")
         elif i==1:
-            packagedTravelled2[i]=float(originToHub)+float(hubToDestination)
-            print("total distance 2: ",packagedTravelled2[i]," meters")
+            packagedTravelled2[j]=float(originToHub)+float(hubToDestination)
+            hubIndex2[j]=packagedTravelled2[j]
+            print("total distance 2: ",packagedTravelled2[j]," meters")
         elif i==2:
-            packagedTravelled3[i]=float(originToHub)+float(hubToDestination)
-            print("total distance 3: ",packagedTravelled3[i]," meters")
+            packagedTravelled3[j]=float(originToHub)+float(hubToDestination)
+            hubIndex3[j]=packagedTravelled3[j]
+            print("total distance 3: ",packagedTravelled3[j]," meters")
+
+
+quick_sort(packagedTravelled1, 0, len(packagedTravelled1) - 1)
+hub1LatAndLong=hubIndex1.index(packagedTravelled1[0])
+print("Customer 1's shortest distance: ",packagedTravelled1[0],"-->",hubs[hub1LatAndLong])
+
+quick_sort(packagedTravelled2, 0, len(packagedTravelled2) - 1)
+hub2LatAndLong=hubIndex2.index(packagedTravelled2[0])
+print("Customer 2's shortest distance: ",packagedTravelled2[0],"-->",hubs[hub2LatAndLong])
+
+quick_sort(packagedTravelled3, 0, len(packagedTravelled3) - 1)
+hub3LatAndLong=hubIndex3.index(packagedTravelled3[0])
+print("Customer 3's shortest distance: ",packagedTravelled3[0],"-->",hubs[hub3LatAndLong])
+
+############## To draw line #########################
+colour=["cornflowerblue","red","orange","green","yellow"]
+gmap = gmplot.GoogleMapPlotter(37.766956, -122.438481, 13, apikey=apikey)
+for i in range(len(customer_ori_lat)):
+    if i==0:
+        hubLat=hub1LatAndLong#hub paling dekat untuk custmer 1 
+        hubLong=hub1LatAndLong
+    elif i==1:
+        hubLat=hub2LatAndLong
+        hubLong=hub2LatAndLong
+    else:
+        hubLat=hub3LatAndLong
+        hubLong=hub3LatAndLong
+    
+    gmap.directions(
+    (customer_ori_lat[i],customer_ori_long[i]),
+    (customer_dest_lat[i],customer_dest_long[i]),
+    waypoints=[(customer_ori_lat[i],customer_ori_long[i]),(hub_lat[hubLat],hub_long[hubLong]),(customer_dest_lat[i],customer_dest_long[i])]
+    )
+
+    gmap.marker(hub_lat[hubLat],hub_long[hubLong],color=colour[hubLat],title=hubs[hubLat])
+    
+gmap.draw('maps.html')
